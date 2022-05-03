0b5a1b923b114419af78bac616453b4720c2f64b
Ovbml <89481220+ovbml@users.noreply.github.com>
2022-05-03 21:46:48 +0300

    Add files via upload

--- #sourse/js/main.js
+++ #sourse/js/main.js
@@ -1,1 +1,16 @@
-// import other .js files: @prepros-append/prepend filename.js
+// to colorize numbers
+Array.prototype.getRandomELement = function() {
+    return this[Math.floor(Math.random() * this.length)];
+}
+Array.prototype.removeElement = function(element) {
+    return this.filter(el => el != element);
+}
+
+let classList = ['_yellow', '_red', '_blue', '_green', '_purple', '_orange', '_pink'];
+
+for ( let number of document.getElementsByClassName('main__number') ) {
+    let className = classList.getRandomELement();
+    classList = classList.removeElement(className);
+
+    number.classList.add(className);
+}
